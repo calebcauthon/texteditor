c57a8d70740c1e6b00ef28759e9d1b744900401f
Caleb Cauthon <calebcauthon@gmail.com>
2019-06-27 07:53:43 -0500

    Add basic test for append

--- editor.rb
+++ editor.rb
@@ -1,3 +1,7 @@
 def text_editor instructions
+  if instructions.length > 0
+    return 'test'
+  end
+
   ''
 end
--- editor_spec.rb
+++ editor_spec.rb
@@ -1,9 +1,17 @@
 require_relative 'editor'
 
 describe 'text editor' do
-  it 'returns a blank string if there are no instructions' do
-    instructions = ''
-    result = text_editor(instructions)
-    expect(result).to eq('')
+  describe 'single instructions' do
+    it 'returns a blank string if there are no instructions' do
+      instructions = ''
+      result = text_editor(instructions)
+      expect(result).to eq('')
+    end
+
+    it 'interprets "1" as append and returns that string' do
+      instructions = '1 test'
+      result = text_editor(instructions)
+      expect(result).to eq('test')
+    end
   end
 end
