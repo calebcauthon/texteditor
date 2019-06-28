583ca0cc8350f866e236b8fe4938f444a97d5aea
Caleb Cauthon <calebcauthon@gmail.com>
2019-06-27 23:01:26 -0500

    Separate the line parsing

--- editor.rb
+++ editor.rb
@@ -1,27 +1,41 @@
-def text_editor instructions
+def text_editor instruction_text
   text_state = TextBuilder.new
-  instruction_lines = instructions.split(/\n/)
-  first_line = instruction_lines.first
+  instructions = Instructions.new
+  instructions.load instruction_text
 
   output = ''
-  if instruction_lines.size > 1
 
-    instruction_lines.slice(1..instruction_lines.size-1).each do |instruction_line|
-      operation = instruction_line.slice(0)
-      if operation == '4'
-        text_state.undo
-      elsif operation == '3'
-        character_index = instruction_line.slice(2..instruction_line.size-1)
-        character = text_state.get_character character_index
-        output = "#{output}#{character}\n"
-      else
-        text_to_append = instruction_line.slice(2..instruction_line.size-1)
-        text_state.append text_to_append
-      end
+  instructions.each do |instruction_line|
+    operation = instruction_line.slice(0)
+    if operation == '4'
+      text_state.undo
+    elsif operation == '3'
+      character_index = instruction_line.slice(2..instruction_line.size-1)
+      character = text_state.get_character character_index
+      output = "#{output}#{character}\n"
+    else
+      text_to_append = instruction_line.slice(2..instruction_line.size-1)
+      text_state.append text_to_append
     end
   end
 
   output
+end
+
+class Instructions
+  def load text
+    all_lines = text.split(/\n/)
+    @instruction_lines = all_lines.slice(1..all_lines.size-1)
+    @index = 0
+  end
+
+  def each
+    return nil unless @instruction_lines.at(@index)
+
+    @instruction_lines.each do |line|
+      yield line
+    end
+  end
 end
 
 class TextBuilder
